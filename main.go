b3c35a75827f6bb8b162284361c691f41f577fcd
takahiro <takapi854@gmail.com>
2022-10-23 22:59:36 +0900

    push

--- main.go
+++ main.go
@@ -1,43 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"golang-apigateway/pkg/cmd"
 )
 
-type Response struct {
-	RequestMethod  string `json:"RequestMethod"`
-	RequestBody    string `json:"RequestBody"`
-	PathParameter  string `json:"PathParameter"`
-	QueryParameter string `json:"QueryParameter"`
-}
-
 func main() {
-	lambda.Start(handler)
-
-}
-
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// httpリクエストの情報を取得
-	method := request.HTTPMethod
-	body := request.Body
-	pathParam := request.PathParameters["pathparam"]
-	queryParam := request.QueryStringParameters["queryparam"]
-
-	// レスポンスとして返すjson文字列を作る
-	res := Response{
-		RequestMethod:  method,
-		RequestBody:    body,
-		PathParameter:  pathParam,
-		QueryParameter: queryParam,
-	}
-	jsonBytes, _ := json.Marshal(res)
-
-	// 返り値としてレスポンスを返す
-	return events.APIGatewayProxyResponse{
-		Body:       string(jsonBytes),
-		StatusCode: 200,
-	}, nil
+	cmd.Run()
 }
